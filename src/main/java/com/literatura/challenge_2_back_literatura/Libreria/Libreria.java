7a5975532215c9157840f205512a450e483bb70d
luimperce16 <luimperce@gmail.com>
2025-01-13 14:53:50 -0500

    first commit

---
+++ src/main/java/com/literatura/challenge_2_back_literatura/Libreria/Libreria.java
@@ -0,0 +1,254 @@
+package com.literatura.challenge_2_back_literatura.Libreria;
+
+import com.literatura.challenge_2_back_literatura.config.ConsumoApiGutendex;
+import com.literatura.challenge_2_back_literatura.config.ConvertirDatos;
+import com.literatura.challenge_2_back_literatura.models.Autor;
+import com.literatura.challenge_2_back_literatura.models.Libro;
+import com.literatura.challenge_2_back_literatura.models.LibrosRespuestaApi;
+import com.literatura.challenge_2_back_literatura.models.records.DatosLibro;
+import com.literatura.challenge_2_back_literatura.repository.iAutorRepository;
+import com.literatura.challenge_2_back_literatura.repository.iLibroRepository;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+public class Libreria {
+
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_GREEN = "\u001B[32m";
+    private static final String ANSI_BLUE = "\u001B[34m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_CYAN = "\u001B[36m";
+
+    private Scanner sc = new Scanner(System.in);
+    private ConsumoApiGutendex consumoApi = new ConsumoApiGutendex();
+    private ConvertirDatos convertir = new ConvertirDatos();
+    private static String API_BASE = "https://gutendex.com/books/?search=";
+    private iLibroRepository libroRepository;
+    private iAutorRepository autorRepository;
+
+    public Libreria(iLibroRepository libroRepository, iAutorRepository autorRepository) {
+        this.libroRepository = libroRepository;
+        this.autorRepository = autorRepository;
+    }
+
+    public void consumo() {
+        var opcion = -1;
+        while (opcion != 0) {
+            var menu = String.format("""
+
+                    %s|***************************************************|%s
+                    %s|*****       BIENVENIDO A LITERALURA          ******|%s
+                    %s|***************************************************|%s
+                    %s
+                    1 - Agregar Libro por Nombre
+                    2 - Libros buscados
+                    3 - Buscar libro por Nombre
+                    4 - Buscar todos los Autores de libros buscados
+                    5 - Buscar Autores por año
+                    6 - Buscar Libros por Idioma
+                    7 - Top 10 Libros más Descargados
+                    8 - Buscar Autor por Nombre
+                    %s
+                    0 - Salir
+                    %s
+                    %s|***************************************************|%s
+                    %s|*****            INGRESE UNA OPCIÓN          ******|%s
+                    %s|***************************************************|%s
+                    """,
+                    ANSI_CYAN, ANSI_RESET, ANSI_GREEN, ANSI_RESET, ANSI_CYAN, ANSI_RESET,
+                    ANSI_YELLOW, ANSI_YELLOW, ANSI_RESET,
+                    ANSI_CYAN, ANSI_RESET, ANSI_CYAN, ANSI_RESET, ANSI_CYAN, ANSI_RESET);
+
+            try {
+                System.out.println(menu);
+                System.out.print(ANSI_BLUE + "Tu opción: " + ANSI_RESET);
+                opcion = sc.nextInt();
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(ANSI_RED + "\n|****************************************|");
+                System.out.println("|  Por favor, ingrese un número válido.  |");
+                System.out.println("|****************************************|\n" + ANSI_RESET);
+                sc.nextLine();
+                continue;
+            }
+
+            switch (opcion) {
+                case 1 -> buscarLibroEnLaWeb();
+                case 2 -> librosBuscados();
+                case 3 -> buscarLibroPorNombre();
+                case 4 -> BuscarAutores();
+                case 5 -> buscarAutoresPorAnio();
+                case 6 -> buscarLibrosPorIdioma();
+                case 7 -> top10LibrosMasDescargados();
+                case 8 -> buscarAutorPorNombre();
+                case 0 -> {
+                    System.out.println(ANSI_GREEN + "\n|********************************|");
+                    System.out.println("|    Aplicación cerrada. Bye!    |");
+                    System.out.println("|********************************|\n" + ANSI_RESET);
+                }
+                default -> {
+                    System.out.println(ANSI_RED + "\n|*********************|");
+                    System.out.println("|  Opción Incorrecta. |");
+                    System.out.println("|*********************|\n" + ANSI_RESET);
+                }
+            }
+        }
+    }
+
+    private Libro getDatosLibro() {
+        System.out.println("Ingrese el nombre del libro: ");
+        var nombreLibro = sc.nextLine().toLowerCase();
+        var json = consumoApi.obtenerDatos(API_BASE + nombreLibro.replace(" ", "%20"));
+        // System.out.println("JSON INICIAL: " + json);
+        LibrosRespuestaApi datos = convertir.convertirDatosJsonAJava(json, LibrosRespuestaApi.class);
+
+        if (datos != null && datos.getResultadoLibros() != null && !datos.getResultadoLibros().isEmpty()) {
+            DatosLibro primerLibro = datos.getResultadoLibros().get(0); // Obtener el primer libro de la lista
+            return new Libro(primerLibro);
+        } else {
+            System.out.println("No se encontraron resultados.");
+            return null;
+        }
+    }
+
+    private void buscarLibroEnLaWeb() {
+        Libro libro = getDatosLibro();
+
+        if (libro == null) {
+            System.out.println("Libro no encontrado. el valor es null");
+            return;
+        }
+
+        // datosLibro.add(libro);
+        try {
+            boolean libroExists = libroRepository.existsByTitulo(libro.getTitulo());
+            if (libroExists) {
+                System.out.println("El libro ya existe en la base de datos!");
+            } else {
+                libroRepository.save(libro);
+                System.out.println(libro.toString());
+            }
+        } catch (InvalidDataAccessApiUsageException e) {
+            System.out.println("No se puede persisitir el libro buscado!");
+        }
+    }
+
+    @Transactional(readOnly = true)
+    private void librosBuscados() {
+        // datosLibro.forEach(System.out::println);
+        List<Libro> libros = libroRepository.findAll();
+        if (libros.isEmpty()) {
+            System.out.println("No se encontraron libros en la base de datos.");
+        } else {
+            System.out.println("Libros encontrados en la base de datos:");
+            for (Libro libro : libros) {
+                System.out.println(libro.toString());
+            }
+        }
+    }
+
+    private void buscarLibroPorNombre() {
+        System.out.println("Ingrese Titulo libro que quiere buscar: ");
+        var titulo = sc.nextLine();
+        Libro libroBuscado = libroRepository.findByTituloContainsIgnoreCase(titulo);
+        if (libroBuscado != null) {
+            System.out.println("El libro buscado fue: " + libroBuscado);
+        } else {
+            System.out.println("El libro con el titulo '" + titulo + "' no se encontró.");
+        }
+    }
+
+    private void BuscarAutores() {
+        // LISTAR AUTORES DE LIBROS BUSCADOS
+        List<Autor> autores = autorRepository.findAll();
+
+        if (autores.isEmpty()) {
+            System.out.println("No se encontraron libros en la base de datos. \n");
+        } else {
+            System.out.println("Libros encontrados en la base de datos: \n");
+            Set<String> autoresUnicos = new HashSet<>();
+            for (Autor autor : autores) {
+                // add() retorna true si el nombre no estaba presente y se añade correctamente
+                if (autoresUnicos.add(autor.getNombre())) {
+                    System.out.println(autor.getNombre() + '\n');
+                }
+            }
+        }
+    }
+
+    private void buscarLibrosPorIdioma() {
+        System.out.println("Ingrese Idioma en el que quiere buscar: \n");
+        System.out.println("|***********************************|");
+        System.out.println("|  Opción - es : Libros en español. |");
+        System.out.println("|  Opción - en : Libros en ingles.  |");
+        System.out.println("|***********************************|\n");
+
+        var idioma = sc.nextLine();
+        List<Libro> librosPorIdioma = libroRepository.findByIdioma(idioma);
+
+        if (librosPorIdioma.isEmpty()) {
+            System.out.println("No se encontraron libros en la base de datos.");
+        } else {
+            System.out.println("Libros segun idioma encontrados en la base de datos:");
+            for (Libro libro : librosPorIdioma) {
+                System.out.println(libro.toString());
+            }
+        }
+
+    }
+
+    private void buscarAutoresPorAnio() {
+        // //BUSCAR AUTORES POR ANIO
+
+        System.out.println("Indica el año para consultar que autores estan vivos: \n");
+        var anioBuscado = sc.nextInt();
+        sc.nextLine();
+
+        List<Autor> autoresVivos = autorRepository
+                .findByCumpleaniosLessThanOrFechaFallecimientoGreaterThanEqual(anioBuscado, anioBuscado);
+
+        if (autoresVivos.isEmpty()) {
+            System.out.println("No se encontraron autores que estuvieran vivos en el año " + anioBuscado + ".");
+        } else {
+            System.out.println("Los autores que estaban vivos en el año " + anioBuscado + " son:");
+            Set<String> autoresUnicos = new HashSet<>();
+
+            for (Autor autor : autoresVivos) {
+                if (autor.getCumpleanios() != null && autor.getFechaFallecimiento() != null) {
+                    if (autor.getCumpleanios() <= anioBuscado && autor.getFechaFallecimiento() >= anioBuscado) {
+                        if (autoresUnicos.add(autor.getNombre())) {
+                            System.out.println("Autor: " + autor.getNombre());
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void top10LibrosMasDescargados() {
+        List<Libro> top10Libros = libroRepository.findTop10ByTituloByCantidadDescargas();
+        if (!top10Libros.isEmpty()) {
+            int index = 1;
+            for (Libro libro : top10Libros) {
+                System.out.printf("Libro %d: %s Autor: %s Descargas: %d\n",
+                        index, libro.getTitulo(), libro.getAutores().getNombre(), libro.getCantidadDescargas());
+                index++;
+            }
+        }
+    }
+
+    private void buscarAutorPorNombre() {
+        System.out.println("Ingrese nombre del escritor que quiere buscar: ");
+        var escritor = sc.nextLine();
+        Optional<Autor> escritorBuscado = autorRepository.findFirstByNombreContainsIgnoreCase(escritor);
+        if (escritorBuscado != null) {
+            System.out.println("\nEl escritor buscado fue: " + escritorBuscado.get().getNombre());
+
+        } else {
+            System.out.println("\nEl escritor con el titulo '" + escritor + "' no se encontró.");
+        }
+    }
+}
